a403375ac885221d6a46fcfa76ba7dd1f4a35b26
Manoj Kumawat <imkumawatmanoj@gmail.com>
2021-11-23 19:31:37 +0530

    Added #3 String Split and Join.py

---
+++ 03 Strings/#3 String Split and Join.py
@@ -0,0 +1,21 @@
+"""
+
+Problem URL:
+https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+Task:
+You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+"""
+
+def split_and_join(line):
+    # write your code here
+    line=line.split(" ")
+    line='-'.join(line)
+    return line
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
+    
