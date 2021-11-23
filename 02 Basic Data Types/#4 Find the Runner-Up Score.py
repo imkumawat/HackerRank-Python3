8eb32e3af97bbed253634055bdae972ea7e268bf
Manoj Kumawat <imkumawatmanoj@gmail.com>
2021-11-23 18:41:09 +0530

    Added #4 Find the Runner-Up Score.py

---
+++ 02 Basic Data Types/#4 Find the Runner-Up Score.py
@@ -0,0 +1,15 @@
+"""
+
+Problem URL:
+https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true
+
+Task:
+Given the participants' score sheet for your University Sports Day,
+you are required to find the runner-up score. You are given n scores.
+Store them in a list and find the score of the runner-up.
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    nums = map(int, input().split())
+    print(sorted(list(set(nums)))[-2])
