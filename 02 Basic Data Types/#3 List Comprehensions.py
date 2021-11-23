c2625bc7fc8aeaeb5d655d1cb876d5f6ef837468
Manoj Kumawat <imkumawatmanoj@gmail.com>
2021-11-23 18:39:20 +0530

    Added #3 List Comprehensions.py

---
+++ 02 Basic Data Types/#3 List Comprehensions.py
@@ -0,0 +1,21 @@
+"""
+
+Problem URL:
+https://www.hackerrank.com/challenges/list-comprehensions/problem?isFullScreen=true
+
+Task:
+Let's learn about list comprehensions! You are given three integers x and y
+representing the dimensions of a cuboid along with an integer n.
+Print a list of all possible coordinates given by (i,j,k) on a 3D grid
+where the sum of (i+j+k) is not equal to n.
+Here, (0<=i<=x; 0<=j<=y; 0<=k<=z). Please use list comprehensions rather than multiple loops, as a learning exercise.
+
+"""
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    print([[a, b, c] for a in range(0, x + 1) for b in range(0, y + 1) for c in range(0, z + 1) if a + b + c != n])
