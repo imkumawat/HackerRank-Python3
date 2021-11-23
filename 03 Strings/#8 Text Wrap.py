18a8a36e0674d07006f6979d8c6c9d0ec97ec523
Manoj Kumawat <imkumawatmanoj@gmail.com>
2021-11-23 20:26:11 +0530

    Added #8 Text Wrap.py

---
+++ 03 Strings/#8 Text Wrap.py
@@ -0,0 +1,19 @@
+"""
+Problem URL:
+https://www.hackerrank.com/challenges/text-wrap/problem?isFullScreen=true
+
+Task:
+You are given a string S and width W.
+Your task is to wrap the string into a paragraph of width W.
+
+"""
+
+import textwrap
+
+def wrap(string, max_width):
+    return textwrap.fill(string,max_width)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
